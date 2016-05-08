2bee6abcfa2156c5c98c41d96d823cd95e41813a
manikantan123 <mani.3ex@gmail.com>
2016-05-08 20:39:39 +0530

    added setters and getters

---
+++ ROLBProject/src/com/rolb/customer/MyDemo.java
@@ -0,0 +1,25 @@
+package com.rolb.customer;
+
+/**
+ * Created by SRIRAM on 5/8/2016.
+ */
+public class MyDemo {
+    private String city;
+    private int DNO;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getDNO() {
+        return DNO;
+    }
+
+    public void setDNO(int DNO) {
+        this.DNO = DNO;
+    }
+}
